a308c8bed2d7585b788e7692cf1f9667bf81f760
nepalialberta <nepalialberta@gmail.com>
2023-03-09 00:11:15 +0000

    Done

--- src/main/java/Application/Service/BookService.java
+++ src/main/java/Application/Service/BookService.java
@@ -40,7 +40,9 @@ public class BookService {
      * @return all books.
      */
     public List<Book> getAllBooks() {
-        return null;
+
+        List<Book> books = bookDAO.getAllBooks();
+        return books;
     }
     /**
      * TODO: Use the bookDAO to persist a book to the database.
@@ -51,15 +53,20 @@ public class BookService {
      * key was already in use.)
      */
     public Book addBook(Book book) {
-
+      
+     
+     if(bookDAO.getBookByIsbn(book.getIsbn())!=null){
         return null;
+     }
+
+        return bookDAO.insertBook(book);
     }
     /**
      * TODO: Use the bookDAO to retrieve a list of all books that have a bookCount above 0.
      * @return all available books (bookCount over zero)
      */
     public List<Book> getAllAvailableBooks() {
-        return null;
+        return bookDAO.getBooksWithBookCountOverZero();
     }
 
 }
